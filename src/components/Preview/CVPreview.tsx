4a671ec950cc63b702e92a53554f144570f1d436
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-09-08 08:42:50 -0300

    :broom: clean: unused files
     Changes to be committed:
    	deleted:    readme-original-vite.md
    	deleted:    src/App-estudo.tsx
    	deleted:    src/components/Form/ExperienceInput.tsx
    	deleted:    src/rascunho.tsx

--- src/components/Preview/CVPreview.tsx
+++ src/components/Preview/CVPreview.tsx
@@ -5,7 +5,7 @@ import ExperienceSection from './ExperienceSection';
 
 const CVPreview: React.FC = () => {
   return (
-    <aside className="w-1/2 p-6 bg-gradient-to-b from-white to-slate-50"> 
+    <aside className="w-1/2 p-6 to-slate-50"> 
       <div className="h-full overflow-y-auto flex justify-center">
         <div id="cv-preview" className="w-[820px] bg-white rounded-xl shadow p-8">
           <PersonalHeader />
